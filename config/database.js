15ab44557455a1e88bc360d95c191c881063d295
brianmichaelanderson <brianmichaelanderson@yahoo.com>
2025-09-25 18:55:03 -0700

    Completed: -database.js for database connection -models/User.js for Mongoose schema -index.js for starting DB followed by starting server

--- config/database.js
+++ config/database.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const mongoURI = process.env.MONGODB_URI;
+
+const connectToDatabase = async () => {
+    try{
+        const conn = await mongoose.connect(mongoURI);
+        console.log('MongoDB connected: ', conn.connection.host)
+    } catch(error){
+        console.error('MongoDB connection error: ', error);
+        throw error;  // calling code handles error
+    }
+};
+
+module.exports = connectToDatabase;
---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const connectToDatabase = require('./config/database');
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try{
+        await connectToDatabase();
+        app.listen(PORT, () => {
+            console.log('Server running on port: ', PORT);
+        });
+    }catch(error){
+        console.error('Failed to start server: ', error);
+        process.exit(1);  // Exit if DB connection fails or server fails to start
+    }
+}
+
+startServer();
