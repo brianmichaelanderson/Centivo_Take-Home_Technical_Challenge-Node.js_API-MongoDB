dc183f918d94cd73157c4276e490788bf2060892
brianmichaelanderson <brianmichaelanderson@yahoo.com>
2025-09-25 20:10:44 -0700

    Working through app.js and routes/users.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-export default app;
+
+//Global error handler
+app.use((err, req, res, next) => {
+    console.error('Global error handler caught: ', err);
+    res.status(500).json({ error: 'Internal Server Error', message: err.message || 'Something went wrong'});
+})
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require(mongoose);
+const User = require('../models/User');
+const router = require('express').Router();
+
+
+
+module.exports = router;
