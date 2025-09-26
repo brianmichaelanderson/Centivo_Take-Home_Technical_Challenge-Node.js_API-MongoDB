79a481d6579ecc96324ca52ce8651051025b23f7
brianmichaelanderson <brianmichaelanderson@yahoo.com>
2025-09-25 21:52:26 -0700

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const userRouter = require('./routes/users');
 
 const app = express();
 app.use(express.json());
 
+app.use('/users', userRouter);
 
 //Global error handler
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
     console.error('Global error handler caught: ', err);
     res.status(500).json({ error: 'Internal Server Error', message: err.message || 'Something went wrong'});
 })
